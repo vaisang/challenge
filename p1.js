02919c691d494a1839271e4e31d260bd3c9e3cbf
Nick Vaisa <Nick Vaisa>
2017-03-20 10:14:58 -0400

    remove punctuation

--- p1.js
+++ p1.js
@@ -23,4 +23,26 @@ describe("A program to implement a crypto square algorithm"), () => {
 	],
 	"stopSpecOnExpectationFailure": false,
 	"random": false
+};
+//remove all spaces from some input text
+var spaceremover = text => text.replace(/\s/g,'');
+
+function punctuationremover(text) {
+	return text.replace(/\W/g,'');
 }
+
+module.exports = spaceremover;
+module.exports = punctuationremover;
+
+//import the spacremover function from our
+//main program file (p1.js)
+var spaceremover = require('../index');
+
+describe("A program to implement a crypto square cypher", () => {
+	.toBe("Anothersentencewithoutspaces.");
+});
+
+it("can remove all punctuation from some text", () => {
+	expect(punctuationremover("Egad! Watson you done fucked up. no punctuation?..."))
+		.toBe("Egad Watson tis this no punctuation");
+});
