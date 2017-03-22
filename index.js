5aa032054ff3545e5db88034dcb030a61befd63b
Nick Vaisa <Nick Vaisa>
2017-03-22 10:58:40 -0400

    normalized

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-// a function to remove punctuation and spaces
-var spacePunc = text => text.replace(/\W/g,'');
-
-//a function to convert all characters to lower case
-var toLower = text => text.toLowercase();
-
 //export all things
 module.exports = {
-	spacePunc: spacePunc,
-	toLower: toLower
+	// a function to remove punctuation and spaces
+	// space and convert to lower case
+
+	normalize: text => text.replace(/\W/g,'').toLowercase();
 };
